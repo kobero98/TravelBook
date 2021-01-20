39b2ecfc9dcef2f8fb830cc004406e47a20c24b2
luchua-bc <shengxin.canada@gmail.com>
2021-01-20 21:25:39 +0000

    Add a salt to SHA256 hash

--- Travelbook/src/main/java/travelbook/controller/ControllerLogin.java
+++ Travelbook/src/main/java/travelbook/controller/ControllerLogin.java
@@ -3,11 +3,11 @@ package main.java.travelbook.controller;
 
 
 import java.sql.SQLException;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
 import java.util.List;
 import java.util.Random;
+
 import javax.mail.MessagingException;
+
 import main.java.travelbook.model.Entity;
 import main.java.travelbook.model.UserEntity;
 import main.java.travelbook.model.bean.RegistrationBean;
@@ -26,30 +26,13 @@ public class ControllerLogin {
 			instance = new ControllerLogin();
 		return instance;
 	}
-	private String passwordHash(String pswd)throws Exception {
-		MessageDigest hasher=MessageDigest.getInstance("SHA-1");
-		hasher.update(pswd.getBytes("UTF-8"));
-		return toHex(hasher.digest());
-	}
-	private static String toHex(byte[] data)  {
-		StringBuilder sb = new StringBuilder();
-		for (byte b : data) {
-			String digit = Integer.toString(b & 0xFF, 16);
- 
-			if (digit.length() == 1) {
-				sb.append("0");
-			}
-			sb.append(digit);
-		}
-		return sb.toString();
-	}
 	public UserBean signIn(String username,String password) throws SQLException{
 		UserBean user=null;
 		PersistanceDAO userDao=DaoFactory.getInstance().create(DaoType.USER);
 		UserEntity userE=new UserEntity();
 		userE.setUsername(username);
 		try {
-		userE.setPassword(this.passwordHash(password));
+		userE.setPassword(password);
 		}catch(Exception e) {
 			throw new SQLException(e.getMessage());
 		}
@@ -90,7 +73,7 @@ public class ControllerLogin {
 		PersistanceDAO userDao= DaoFactory.getInstance().create(DaoType.USER);
 		UserEntity newUser= new UserEntity(user);
 		try {
-		newUser.setPassword(this.passwordHash(user.getPassword()));
+		newUser.setPassword(user.getPassword());
 		}catch(Exception e) {
 			e.getStackTrace();
 		}
