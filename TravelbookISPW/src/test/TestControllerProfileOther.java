7455a6d6e54ba5c894bb9ce7fb961286a6bb379e
Sara-DaCanal <57182491+Sara-DaCanal@users.noreply.github.com>
2021-02-09 19:05:28 +0100

    risolti dei problemi

--- TravelbookISPW/src/test/TestControllerProfileOther.java
+++ TravelbookISPW/src/test/TestControllerProfileOther.java
@@ -1,5 +1,28 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import exception.DBException;
+import main.java.travelbook.controller.ControllerProfileOther;
+import main.java.travelbook.model.bean.UserBean;
+
+/*
+ * @author Sara Da Canal
+ */
 public class TestControllerProfileOther {
-//TODO
+	@Test
+	public void testUpdateFollow() {
+		ControllerProfileOther controller = new ControllerProfileOther();
+		UserBean u = new UserBean(6);
+		List<Integer> f = new ArrayList<>();
+		f.add(19);
+		u.setFollowing(f);
+		//assertThrows(DBException.class, ()->controller.updateFollow(u));
+	}
+	
 }
